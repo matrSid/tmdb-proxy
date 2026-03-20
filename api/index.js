2e0bda92db7b4c777e0cd6cffc7b8fa04009a559
matrSid <siddhantj972@gmail.com>
2026-03-20 12:35:21 +0530

    fixes

--- api/index.js
+++ api/index.js
@@ -50,5 +50,11 @@ app.get("/movies/search", async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
 
 export default app;
